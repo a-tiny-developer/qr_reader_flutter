3f911a1395a2178143f196bb437023d10ae51bf7
a-tiny-developer <atinyflame@gmail.com>
2022-02-15 15:56:48 -0600

    ScanModel

---
+++ lib/models/scan_model.dart
@@ -0,0 +1,31 @@
+import 'dart:convert';
+
+class ScanModel {
+  ScanModel({
+    required this.id,
+    required this.type,
+    required this.value,
+  }) {
+    type = type.contains('https') ? 'https' : 'geo';
+  }
+
+  int id;
+  String type;
+  String value;
+
+  factory ScanModel.fromJson(String str) => ScanModel.fromMap(json.decode(str));
+
+  String toJson() => json.encode(toMap());
+
+  factory ScanModel.fromMap(Map<String, dynamic> json) => ScanModel(
+        id: json["id"],
+        type: json["type"],
+        value: json["value"],
+      );
+
+  Map<String, dynamic> toMap() => {
+        "id": id,
+        "type": type,
+        "value": value,
+      };
+}
