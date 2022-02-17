56b4f349d146297e3cfbef8d0433b756e749d6f8
a-tiny-developer <atinyflame@gmail.com>
2022-02-16 18:58:51 -0600

    Show google map

--- lib/screens/map_screen.dart
+++ lib/screens/map_screen.dart
@@ -1,8 +1,23 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:qr_reader_flutter/models/models.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class MapScreen extends StatelessWidget {
+class MapScreen extends StatefulWidget {
   const MapScreen({Key? key}) : super(key: key);
+
+  @override
+  State<MapScreen> createState() => _MapScreenState();
+}
+
+class _MapScreenState extends State<MapScreen> {
+  final Completer<GoogleMapController> _controller = Completer();
+
+  static const CameraPosition startingPoint = CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 14.4746,
+  );
 
   @override
   Widget build(BuildContext context) {
@@ -12,8 +27,12 @@ class MapScreen extends StatelessWidget {
         centerTitle: true,
         title: const Text('Map'),
       ),
-      body: Center(
-        child: Text(scan.value),
+      body: GoogleMap(
+        mapType: MapType.hybrid,
+        initialCameraPosition: startingPoint,
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+        },
       ),
     );
   }
